713f14d5713277a5603b7709d18d51349fc03d07
Tuan <Neyugntuana9@gmail.com>
2023-10-25 22:31:16 +0700

    c2 video3

---
+++ video3/video3.js
@@ -0,0 +1,11 @@
+//const x = 5;
+//x = 6;
+
+ console.log('Hello world from javascript: ')
+
+ let x = 5 , y = 6 , z = 7;
+
+
+ console.log("Test string" ,'x = ', x ,'y = ',y ,'z = ',z) //javascipt style
+
+ console.log("Test string2" + ' x = '+ x +' y = '+y +' z = '+z) //java style 
