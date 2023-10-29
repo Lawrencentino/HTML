2cc026ac3aac72ef038c7d88f9bb02e7dedbbfb9
Tuan <Neyugntuana9@gmail.com>
2023-10-29 19:55:06 +0700

    c2 video7

---
+++ video7/video7.js
@@ -0,0 +1,15 @@
+console.log(`Hello world from HTML`)
+
+let a = 5;
+let b = 10;
+console.log(`Fifteen is " + $(a + b) + " and
+not " + $(2 * a + b) + ".`);
+
+
+//url: `/hoidanit/5
+console.log(`Hoi dan IT check strings : a = ${a} b = ${b}  a+b = ${a+b}`) //dấu dollar : khai báo biến
+console.log(`/hoidanit/${a}`)
+
+
+let n =`Mười trừ năm bằng : `
+console.log(`mười trừ năm bằng : ${b-a}`)
