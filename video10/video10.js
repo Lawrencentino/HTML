88b1f9a5b66e19f9cb251f606a8f7bbd2f992136
Tuan <Neyugntuana9@gmail.com>
2023-10-31 20:46:08 +0700

    c2 video10

---
+++ video10/video10.js
@@ -0,0 +1,11 @@
+console.log(`Hello world from HTML`)
+let a = 5; // x <- y : lấy giá trị y gán cho x
+let b =`5`;
+
+console.log(`compare a,b : `,` a = `,a ,` b = `,b ,` result `,a === b)//dùng === sẽ chính xác hơn:num,str
+// === : data type ,value
+// == : value
+
+//empty ,null, undefined
+let c = null;
+console.log(`Check value: `,c)
